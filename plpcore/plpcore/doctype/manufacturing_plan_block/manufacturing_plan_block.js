f07c36ef841990be1dd3f7dc82e07a6410d25eef
lamphankhoi <lamphankhoi@gmail.com>
2021-07-20 11:12:34 +0700

    PLPCORE

---
+++ plpcore/plpcore/doctype/manufacturing_plan_block/manufacturing_plan_block.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, PLP and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Manufacturing Plan Block', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ plpcore/plpcore/doctype/outdoor_eatery/outdoor_eatery.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, PLP and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Outdoor Eatery', {
+	// refresh: function(frm) {
+
+	// }
+});
